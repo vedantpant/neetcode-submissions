940525e8f5dd15afcc01569b2359235d716016dc
vedantpant <54245823+vedantpant@users.noreply.github.com>
2026-05-29 09:55:22 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        
+        for str in strs:
+            key = "".join(sorted(str))
+            groups[key].append(str)
+
+        return list(groups.values())
+
+        
